68a193e12c3aad67a7bd6fa9cf85cb8867a66e65
ZZQ <55095544@qq.com>
2016-06-25 14:48:17 +0800

    ZZQ music 请求数据 MODEL

--- ShowNews/ShowNews/classes/Marco/NewsUrl.h
+++ ShowNews/ShowNews/classes/Marco/NewsUrl.h
@@ -15,7 +15,13 @@
 
 
 // 音乐搜索的URL
-
-#define NEWS_MUSIC_SEARCH_URL(aaa,bbb,ccc) @"http://s.music.qq.com/fcgi-bin/music_search_new_platform?t=0&n=##bbb&aggr=1&cr=1&loginUin=##ccc&format=json&inCharset=GB2312&outCharset=utf-8&notice=0&platform=jqminiframe.json&needNewCode=0&p=##aaa&catZhida=0&remoteplace=sizer.newclient.next_song&w=0"
+//#define str @"http://s.music.qq.com/fcgi-bin/music_search_new_platform?t=0&n=
+//#define str1(bbb) #bbb
+//#define str2 @"#&aggr=1&cr=1&loginUin="
+//#define str3(ccc) #ccc
+//#define str4 @"&format=json&inCharset=GB2312&outCharset=utf-8&notice=0&platform=jqminiframe.json&needNewCode=0&p="
+//#define str5 (aaa) #aaa
+//#define str6 @"&catZhida=0&remoteplace=sizer.newclient.next_song&w=0"
+#define NEWS_MUSIC_SEARCH_URL231 @"http://s.music.qq.com/fcgi-bin/music_search_new_platform?t=0&n=20&aggr=1&cr=1&loginUin=0&format=json&inCharset=GB2312&outCharset=utf-8&notice=0&platform=jqminiframe.json&needNewCode=0&p=1&catZhida=0&remoteplace=sizer.newclient.next_song&w=%s"
 
 #endif /* NewsUrl_h */
---
+++ ShowNews/ShowNews/classes/Scences/Music/Model/Music.m
@@ -0,0 +1,13 @@
+//
+//  Music.m
+//  ShowNews
+//
+//  Created by ZZQ on 16/6/25.
+//  Copyright © 2016年 YZZL. All rights reserved.
+//
+
+#import "Music.h"
+
+@implementation Music
+
+@end
