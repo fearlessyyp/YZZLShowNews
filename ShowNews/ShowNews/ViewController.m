a22b42ca696e65e625e496bbd92989fdc529fa66
ZZQ <55095544@qq.com>
2016-06-25 11:56:21 +0800

    ZZQ  viewconller  音乐控制器

--- ShowNews/ShowNews/ViewController.m
+++ ShowNews/ShowNews/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 #import "NewsController.h"
 #import "VideoTableViewController.h"
+#import "MusicSearchController.h"
 @interface ViewController ()
 
 @end
@@ -18,7 +19,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    VideoTableViewController *VTVC = [[VideoTableViewController alloc] init];
+//    VideoTableViewController *VTVC = [[VideoTableViewController alloc] init];
+//    [self.navigationController pushViewController:VTVC animated:YES];
+    MusicSearchController *VTVC = [[MusicSearchController alloc] init];
     [self.navigationController pushViewController:VTVC animated:YES];
 }
 
