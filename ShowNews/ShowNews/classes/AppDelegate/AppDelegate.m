7e8e8e0bfdf4e2fda9fad76b15110c7732326cba
ZZQ <55095544@qq.com>
2016-07-07 19:43:30 +0800

    周子琦 进入后台清楚图片缓存

--- ShowNews/ShowNews/classes/AppDelegate/AppDelegate.m
+++ ShowNews/ShowNews/classes/AppDelegate/AppDelegate.m
@@ -200,6 +200,9 @@
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     [application beginReceivingRemoteControlEvents];
+    [[SDImageCache sharedImageCache] clearDisk];
+    [[SDImageCache sharedImageCache] clearMemory];
+  
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
@@ -215,6 +218,7 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+   
 }
 
 -(void)remoteControlReceivedWithEvent:(UIEvent *)event{
